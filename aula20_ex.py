2204a7b4b634a62605aea16a83a12fbe22fbea0f
nicolasdca <nicolasdavid193@gmail.com>
2025-02-20 13:58:09 -0300

    exercise 1: logical operators and conditions

---
+++ aula20_ex.py
@@ -0,0 +1,7 @@
+valor_1 = input('Primeiro valor: ')
+valor_2 = input('Segundo valor: ')
+
+if valor_1 > valor_2:
+    print(f'{valor_1} é maior que {valor_2}')
+else:
+    print(f'{valor_1} não é maior que {valor_2}')
